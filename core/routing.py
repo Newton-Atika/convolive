7cd78f961503f70bd220850c5482961e5de3257a
Newton-Atika <163111440+Newton-Atika@users.noreply.github.com>
2025-07-17 11:58:18 +0300

    Update routing.py

--- core/routing.py
+++ core/routing.py
@@ -1,6 +1,5 @@
 from django.urls import re_path
 from . import consumers
-
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<event_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/event/(?P<event_id>\d+)/$', consumers.EventLikeConsumer.as_asgi()),
