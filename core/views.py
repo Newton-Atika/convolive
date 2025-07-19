043d44aac265bf731e49e568f3e00b5c8e0d43fe
Newton-Atika <163111440+Newton-Atika@users.noreply.github.com>
2025-07-19 12:04:41 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -54,6 +54,33 @@ import jwt
 import time
 import os
 from django.conf import settings
+from django.http import JsonResponse
+from agora_token_service import RtcTokenBuilder
+import time
+import os
+
+AGORA_APP_ID = "5a7551a1892a47258b7e9f7f264e6196"
+AGORA_APP_CERTIFICATE = "27b20c8f267e4235b207d6aef1bf7dea"
+
+def generate_agora_token(request):
+    channel_name = request.GET.get("channel")
+    uid = request.GET.get("uid", "0")  # 0 for default
+    role = request.GET.get("role", "publisher")  # 'publisher' or 'subscriber'
+
+    if role == "publisher":
+        role_enum = 1
+    else:
+        role_enum = 2
+
+    expiration_time_in_seconds = 3600
+    current_timestamp = int(time.time())
+    privilege_expired_ts = current_timestamp + expiration_time_in_seconds
+
+    token = RtcTokenBuilder.buildTokenWithUid(
+        AGORA_APP_ID, AGORA_APP_CERTIFICATE, channel_name, int(uid), role_enum, privilege_expired_ts
+    )
+
+    return JsonResponse({"token": token, "appId": AGORA_APP_ID})
 
 
 def create_token(identity, room, can_publish=False):
