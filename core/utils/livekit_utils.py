78d2a6dfe5d7620ed80a39b3d56857c9023226b1
Newton-Atika <163111440+Newton-Atika@users.noreply.github.com>
2025-07-19 07:29:06 +0300

    Update livekit_utils.py

--- core/utils/livekit_utils.py
+++ core/utils/livekit_utils.py
@@ -2,10 +2,10 @@
 import os
 from livekit import api
 
-LIVEKIT_URL = os.getenv("LIVEKIT_URL")
-LIVEKIT_API_KEY = os.getenv("LIVEKIT_API_KEY")
-LIVEKIT_API_SECRET = os.getenv("LIVEKIT_API_SECRET")
-
+LIVEKIT_URL = 'wss://convolive-1v7m4e6q.livekit.cloud'
+LIVEKIT_API_KEY = "API3mLfygt68Dxs"
+LIVEKIT_API_SECRET = "nyDCJgfJ2OF8daESzSje8rrVJnZBWFzv5vXKZNeqF1oA"
+WEB_SOCKET_URL = "wss://convolive-1v7m4e6q.livekit.cloud"
 def create_token(identity: str, room: str, publish: bool):
     at = api.AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET)
     at.with_identity(identity).with_name(identity)
