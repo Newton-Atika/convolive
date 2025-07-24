216b2460479d2d793d88fe1806ed24e30980bb3a
Newton-Atika <163111440+Newton-Atika@users.noreply.github.com>
2025-07-24 14:26:41 +0300

    Update routing.py

--- core/routing.py
+++ core/routing.py
@@ -3,5 +3,6 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<event_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/event/(?P<event_id>\d+)/$', consumers.EventLikeConsumer.as_asgi()),
-    re_path(r'ws/stream/(?P<event_id>\d+)/$', consumers.StreamConsumer.as_asgi()),  # ADD THIS LINE
+    re_path(r'ws/stream/(?P<event_id>[\w\-]+)/$', consumers.StreamConsumer.as_asgi())
+  # ADD THIS LINE
 ]
